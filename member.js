96c7304a51a2d9272781c1cb312669bd6951cc2d
MesssitO <151827916+MesssitO@users.noreply.github.com>
2023-12-04 07:26:26 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,20 @@
+function skillsMembers() {
+  return [
+    {
+      name: 'John',
+      skills: ['JavaScript', 'CSS', 'HTML', 'React']
+    },
+    {
+      name: 'Jane',
+      skills: ['JavaScript', 'CSS', 'HTML', 'Angular']
+    },
+    {
+      name: 'Jack',
+      skills: ['JavaScript', 'CSS', 'HTML', 'React']
+    },
+    {
+      name: 'Jill',
+      skills: ['JavaScript', 'CSS', 'HTML', 'Angular']
+    }
+  ]
+}
